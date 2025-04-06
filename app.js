87d3d40ed710ea455011199577fff0188bfe00f1
shreyasKU31 <shreyasku31@gmail.com>
2025-04-06 09:28:11 +0530

    Handled error on client and server

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing.js");
-var methodOverride = require("method-override");
+const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -36,80 +38,106 @@ app.get("/", (req, res) => {
 app.get("/listings/new", (req, res) => {
   res.render("createList.ejs");
 });
-app.post("/listings", async (req, res) => {
-  const { title, discription, image, price, location, country } = req.body;
-  let listing = new Listing({
-    title,
-    discription,
-    image,
-    price,
-    location,
-    country,
-  });
-  await listing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body) {
+      throw new ExpressError(
+        400,
+        "Ivalid form data. Please fill all the fields."
+      );
+    }
+    const { title, discription, image, price, location, country } = req.body;
+    let listing = new Listing({
+      title,
+      discription,
+      image,
+      price,
+      location,
+      country,
+    });
+    await listing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Route to get all the listings data from DB
-app.get("/listings", async (req, res) => {
-  await Listing.find().then((r) => {
-    let listings = r;
-    res.render("listings.ejs", { listings });
-  });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    await Listing.find().then((r) => {
+      let listings = r;
+      res.render("listings.ejs", { listings });
+    });
+  })
+);
 
 //Route to show a listing in detail
-app.get("/listings/:id", async (req, res) => {
-  const { id } = req.params;
-  await Listing.findById(id)
-    .then((r) => {
-      const list = r;
-      res.render("list.ejs", { list });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    await Listing.findById(id)
+      .then((r) => {
+        const list = r;
+        res.render("list.ejs", { list });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+);
 
 //Edit route
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findById(id)
-    .then((r) => {
-      let editList = r;
-      res.render("edit.ejs", { editList });
-    })
-    .catch((err) => console.log(err));
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findById(id)
+      .then((r) => {
+        let editList = r;
+        res.render("edit.ejs", { editList });
+      })
+      .catch((err) => console.log(err));
+  })
+);
 //Udate Route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  // console.log(req.body);
-  await Listing.findByIdAndUpdate(id, req.body);
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body) {
+      throw new ExpressError(
+        400,
+        "Ivalid form data. Please fill all the fields."
+      );
+    }
+    let { id } = req.params;
+    // console.log(req.body);
+    await Listing.findByIdAndUpdate(id, req.body);
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 //Delete route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect("/listings");
-});
-//Route to test DB listing
-// app.get("/testlisting", async (req, res) => {
-//   let listing = new Listing({
-//     title: "Test Listing",
-//     description: "this is a hotel in banglore near majestic",
-//     image: "",
-//     price: 3000,
-//     location: "Banglore",
-//     country: "India",
-//   });
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  })
+);
 
-//   await listing.save();
-//   console.log("Listing is done");
-//   res.send("Yes happy listing");
-// });
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not Found"));
+});
+
+//Error Route
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something is wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+});
+
 app.listen(8000, () => {
   console.log("Server is running at port 8000");
 });
